ba83a526afa058b0901ebca44f5010b954487c29
randhir200 <mail.randhirkr@gmail.com>
2024-05-07 00:14:36 +0530

    health check route added in api

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ const express = require('express');
 require('dotenv').config();
 const connection = require('./config/db');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const logger = require('./logger');
 const masterRoute = require('./routes/masterRoute');
 
 app.use(express.json());
-app.use('/dummy', (req, res)=>{
-    res.send('Respons Get Back');
-})
+app.use(
+  cors({
+    origin: "",
+    methods: "GET,POST",
+    credentials: true, // allow session cookie from browser to pass through
+    exposedHeaders: ["auth-token"],
+  })
+);
+
 app.use('/', masterRoute);
 
+app.get("/health", async (req, res) => {
+  const mongoState = mongoose.STATES[mongoose.connection.readyState];
 
+  if (mongoState === "connected") {
+    logger.info(`mongoDb connection: up, ${mongoState}`);
+    res.status(200).json({ status: "up", dbState: mongoState });
+  } else {
+    logger.info(`mongoDb connection: down, ${mongoState}`);
+    res.status(500).json({ status: "down", dbState: mongoState });
+  }
+});
 
 const port = process.env.PORT || 7000;
 app.listen(port, async () => {
---
+++ logger.js
@@ -0,0 +1,32 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json()
+    ),
+    defaultMeta: { service: 'Invo Manage' },
+    transports: [
+        // Write all logs with importance level of `error` or less to `error.log`
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+
+        // Write all logs with importance level of `info` or less to `combined.log`
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+// If we're not in production, log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }));
+}
+
+
+module.exports = logger

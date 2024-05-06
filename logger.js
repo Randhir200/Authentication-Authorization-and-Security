a88fa78ef203df1928817fa3637c780a65ad44b1
randhir200 <mail.randhirkr@gmail.com>
2024-05-07 00:22:43 +0530

    unwated code removed from logger

--- logger.js
+++ logger.js
@@ -20,13 +20,7 @@ const logger = winston.createLogger({
     ],
 });
 
-// If we're not in production, log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple(),
-    }));
-}
+
 
 
 module.exports = logger

f1c73bce9a968942e2e85256fe2fb0c5512e2db7
Randhir Kumar <92780081+Randhir200@users.noreply.github.com>
2024-11-01 10:38:12 +0530

    removed sensitive data

--- config/db.js
+++ config/db.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const DB = process.env.DB_URL.replace('<username>', process.env.DB_USER)
           .replace('<password>', process.env.DB_PASS);
 
-// const uri = "mongodb+srv://mailrandhirkr:Budget00@cluster.bkudr.mongodb.net/BugetBuudy";
-
 const connection = async () => {
   try{
     await mongoose.connect(DB);

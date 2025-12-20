2feb6cfbdc73e4b57f704abd3aab6f9df46f6b2c
Randhir Kumar <92780081+Randhir200@users.noreply.github.com>
2025-12-20 20:00:54 +0000

    response method corrected!

--- utils/response.js
+++ utils/response.js
@@ -8,7 +8,7 @@ const statusMap = {
     internalError: 500,
 };
 
-exports.response = (res, status, message, data=[])=>{
+exports.response = (res, status, message, data)=>{
     res.set('Cache-Control', 'public, max-age=604800'); // cache for 7 days
     res.set('X-Content-Type-Options', 'nosniff'); // prevent MIME type sniffing
     res.set('X-Frame-Options', 'DENY'); // prevent clickjacking
@@ -17,7 +17,7 @@ exports.response = (res, status, message, data=[])=>{
     return res.status(statusMap[status]||404).json({
         status,
         message,
-        data, // avoid sending null
+        data: data ?? undefined,
         statusCode: statusMap[status]
     })
 } 
